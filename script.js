6dd623b82f2da127ef74ccac10f00d398420c285
sinannotificacao <sinannotificacao@gmail.com>
2026-04-07 22:08:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.14 });
 
 document.querySelectorAll('.reveal').forEach((el) => observer.observe(el));
+
+
+// --- MENU INTELIGENTE ---
+let lastScroll = 0;
+const header = document.getElementById("topbar");
+
+window.addEventListener("scroll", () => {
+  let current = window.pageYOffset;
+
+  if (current > lastScroll && current > 80){
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  if(current > 50){
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+
+  lastScroll = current;
+});
