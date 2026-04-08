e06f42864232691c463d47a807758740bbef0138
sinannotificacao <sinannotificacao@gmail.com>
2026-04-07 22:01:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,19 +3,13 @@ let lastScroll = 0;
 const header = document.getElementById("topbar");
 
 window.addEventListener("scroll", () => {
-  let current = window.pageYOffset;
+  let currentScroll = window.pageYOffset;
 
-  if (current > lastScroll && current > 80){
+  if (currentScroll > lastScroll && currentScroll > 80) {
     header.classList.add("hide");
   } else {
     header.classList.remove("hide");
   }
 
-  if(current > 50){
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-
-  lastScroll = current;
+  lastScroll = currentScroll;
 });
