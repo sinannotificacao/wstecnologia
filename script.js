d2203ebe2d616748c90ace9d5a8ea67dbd1e557b
sinannotificacao <sinannotificacao@gmail.com>
2026-04-13 16:45:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Validação de formulário de contato
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Formulário enviado com sucesso!');
+});
