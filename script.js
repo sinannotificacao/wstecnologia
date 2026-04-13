bd49905858da8e9f1270c4d9f8a8c72406a0cebb
sinannotificacao <sinannotificacao@gmail.com>
2026-04-13 17:07:40 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-// Validação de formulário de contato
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Formulário enviado com sucesso!');
-});
